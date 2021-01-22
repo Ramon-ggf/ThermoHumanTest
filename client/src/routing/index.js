0eea2ba354d7040fcc3f0b98327eb28ae5ab900e
Ramon Rodriguez <ramon.frlopez@gmail.com>
2021-01-22 14:24:04 +0100

    feat: protected user profile

--- client/src/routing/index.js
+++ client/src/routing/index.js
@@ -3,7 +3,7 @@ import { Switch, Route, useHistory } from "react-router-dom"
 import { Auth0Provider } from "@auth0/auth0-react";
 
 import { Login, Profile } from "./../views"
-import AuthGuard from "./../guards";
+
 
 const Routing = () => {
 
@@ -18,17 +18,12 @@ const Routing = () => {
             domain="dev-eqjie6wk.eu.auth0.com"
             clientId="rhaSTRjVlOJyGmNx6RpphxcT4pQZoH5R"
             redirectUri={window.location.origin}
-            onRedirectCallback={onRedirectCallback}
-        >
+            onRedirectCallback={onRedirectCallback}>
             <Switch>
                 <Route path={"/login"} component={Login} />
-
-
                 <Route path={"/profile"} component={Profile} />
-
             </Switch>
         </Auth0Provider>
-
     )
 }
 
