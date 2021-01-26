9c082427829c70e88193cd9262389bbe430cffb7
Ramon Rodriguez <ramon.frlopez@gmail.com>
2021-01-26 14:57:26 +0100

    logo

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,11 +1,21 @@
 import { Link } from "react-router-dom"
 import { Navbar, Nav } from "react-bootstrap"
 
+import Logo from "./logo-thermo.png"
+
 const NavMenu = () => {
     return (
         <Navbar className="navbar" collapseOnSelect expand="md" bg="dark" variant="dark">
             <Link to={"/"} style={{ padding: 10, color: "#7B698C", border: "2px solid", textDecoration: "none", fontWeight: 600 }}>
-                <Navbar.Brand >Thermo Human Test</Navbar.Brand>
+                <Navbar.Brand >
+                    <img
+                        alt="Logotipo"
+                        src={Logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}Thermo Human Test
+                </Navbar.Brand>
             </Link>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
